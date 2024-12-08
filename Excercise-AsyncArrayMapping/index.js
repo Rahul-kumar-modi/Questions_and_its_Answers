d30d9c4a73170d5a03c5a9a88dbcb4653cc663f7
Rahul modi <rahulkumarmodi.8294@gmail.com>
2024-12-08 23:01:18 +0530

    Update index.js

--- Excercise-AsyncArrayMapping/index.js
+++ Excercise-AsyncArrayMapping/index.js
@@ -1,24 +1,25 @@
-// let arr = [1, 2, 3, 4, 5, 6]
-// let newArr = []
-// async function sum() {
-//     return new Promise((resolve, reject) => {
-//         setInterval(() => {             
-//         for (let i = 0; i < arr.length; i++) {
-//             newArr.push(arr[i] * 2)
+//1st method
+let arr = [1, 2, 3, 4, 5, 6]
+let newArr = []
+async function sum() {
+    return new Promise((resolve, reject) => {
+        setInterval(() => {             
+        for (let i = 0; i < arr.length; i++) {
+            newArr.push(arr[i] * 2)
                 
-//         }
-//         resolve(newArr)
-//     }, 500);
-//     })
-// }
+        }
+        resolve(newArr)
+    }, 500);
+    })
+}
 
-// async function main() {
-//     let data = await sum()
+async function main() {
+    let data = await sum()
 
-//     console.log(data);
-// }
+    console.log(data);
+}
 
-// main()
+main()
 
 //2nd method
 let arr1 = [1, 2, 3, 4, 5, 6]
